03b96cea9c253a39b0be1f59a04f350fbebeb918
dpp17 <durga123q.com>
2023-01-30 21:22:50 +0530

    Added: add new contact feature added

--- src/addressbook/services/ImplementationsOfAddressBook.java
+++ src/addressbook/services/ImplementationsOfAddressBook.java
@@ -1,9 +1,13 @@
 package addressbook.services;
 
 import addressbook.interfaces.InterfaceAddressBook;
+import addressbook.model.Contacts;
 
 public class ImplementationsOfAddressBook implements InterfaceAddressBook{
 
+	
+	Contacts[] storeContactDelails = new Contacts[15];//array
+	
 	public void welcomeDisplay() {
 		System.err.println("===================================");
 		System.out.println("  ::  Welcome To Address_Book  ::  ");
@@ -11,5 +15,19 @@ public class ImplementationsOfAddressBook implements InterfaceAddressBook{
 		
 	}
 
-
+	public int createContactDetails(Contacts contacts) {
+		System.out.println(" List of All the Contscts ");
+		int index = getEmptyIndexToStore();
+		storeContactDelails[index] = contacts;
+		return index;
+	}
+	
+	private int getEmptyIndexToStore() {
+		for(int i = 0; i <storeContactDelails.length; i++) {
+				if(storeContactDelails[i] == null) {
+				return i;
+			}
+		}
+		return 0;
+	}
 }
