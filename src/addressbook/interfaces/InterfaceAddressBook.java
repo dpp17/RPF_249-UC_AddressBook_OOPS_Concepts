878104ab9dbd81e2760d40e15f34ad465ec814c4
dpp17 <durga123q.com>
2023-01-30 21:22:19 +0530

    Added: add new contact feature added

--- src/addressbook/interfaces/InterfaceAddressBook.java
+++ src/addressbook/interfaces/InterfaceAddressBook.java
@@ -1,6 +1,9 @@
 package addressbook.interfaces;
 
+import addressbook.model.Contacts;
+
 public interface InterfaceAddressBook {
 
 	void welcomeDisplay();
+	int createContactDetails(Contacts contacts);
 }
